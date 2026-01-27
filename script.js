7e22de840597275093df37a64732efd32d3c6863
177102proaplweb-tech <177102proaplweb@gmail.com>
2026-01-27 08:56:54 +0000

    cleaned up

--- script.js
+++ script.js
@@ -1,36 +1,11 @@
-// const example = document.getElementById('example')
-// const answer = document.getElementById('answer')
-// const capital = document.getElementById('capital')
-// example.addEventListener("click", function() {
-//   let cap = capital.value
-//   if (cap == "") cap = "Warsaw"
-//   let table = document.createElement('table')
-//   table.innerHTML = "<tr><th>Name</th><th>Capital</th><th>Population</th><th>Region</th><th>Subregion</th></tr>"
-//   fetch(`https://restcountries.com/v3.1/capital/${cap}`)
-//     .then(response => response.json())
-//     .then(array => {
-//       console.log(array)
-//       for (let i = 0; i < array.length; i++) {
-//         let item = document.createElement('tr')
-//         item.innerHTML = `<td>${array[i].name.common}</td><td>${array[i].capital[0]}</td><td>${array[i].population}</td><td>${array[i].region}</td><td>${array[i].subregion}</td>`
-//         table.appendChild(item)
-//         // console.log("userID:", array[i].userId + ".", "id:", array[i].id + '.', "title:", array[i].title + '.')
-//       }
-//     })
-//   answer.innerHTML = ""
-//   answer.appendChild(table)
-// })
-
 const createLobbyButton = document.getElementById("createLobby");
 const createdLobbyCode = document.getElementById("createdLobbyCode");
-// const checkTurnButton = document.getElementById("checkTurn");
 const lobbyDiv = document.getElementById("lobbyDiv");
 const playAreaDiv = document.getElementById("playAreaDiv");
 const tableCard = document.getElementById("tableCard");
 const pileCard = document.getElementById("pileCard");
 
 const joinLobbyButton = document.getElementById("joinLobby");
-// const passTurnButton = document.getElementById("passTurn");
 const joinLobbyCode = document.getElementById("joinLobbyCode");
 
 const handSizeInput = document.getElementById("handSizeInput");
@@ -60,8 +35,6 @@ function endTurn() {
       myTurn = 0;
       turnText.innerHTML = "Opponent's turn";
       hold = 2;
-      console.log("Moved joker");
-      console.log(array);
     });
 }
 
@@ -88,8 +61,6 @@ function playCard(e) {
           .then((array) => {
             myTurn = 1;
             turnText.innerHTML = "You win!";
-            // console.log("Moved joker");
-            // console.log(array);
           });
       }
     });
@@ -111,7 +82,6 @@ function drawCards(n) {
         handHTML.appendChild(img);
       });
       if (array.cards.length < n) {
-        // TODO: reshuffle cards
         fetch(
           `https://deckofcardsapi.com/api/deck/${deckID}/pile/table/return/`,
         ).then(
@@ -129,44 +99,23 @@ function drawCards(n) {
     });
 }
 
-// passTurnButton.addEventListener("click", function () {
-//   if (deckID == null || myTurn == 0) return;
-//   console.log("Passing turn");
-//   fetch(
-//     `https://deckofcardsapi.com/api/deck/${deckID}/pile/no_jokers/add/?cards=X1`,
-//   )
-//     .then((response) => response.json())
-//     .then((array) => {
-//       myTurn = 0;
-//       console.log("Moved joker");
-//       console.log(array);
-//     });
-// });
-
 joinLobbyButton.addEventListener("click", function () {
   deckID = joinLobbyCode.value;
   handHTML.replaceChildren();
   if (deckID == "") return;
-  console.log("Joining lobby with ID:", deckID);
   fetch(`https://deckofcardsapi.com/api/deck/${deckID}/shuffle/?remaining=true`)
     .then((response) => response.json())
     .then((array) => {
-      console.log(array);
       if (!array.success) {
-        console.log("Wrong lobby code!");
         return;
       }
       startingHandSize = 52 - array.remaining;
-      console.log("Joined lobby with hand size", startingHandSize);
       fetch(
         `https://deckofcardsapi.com/api/deck/${deckID}/pile/no_jokers/add/?cards=X1`,
       )
         .then((response) => response.json())
         .then((array) => {
-          console.log("Moved joker");
-          console.log(array);
           hold = 5;
-          console.log(`Drawing ${startingHandSize} cards`);
           fetch(
             `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${startingHandSize}`,
           )
@@ -188,7 +137,6 @@ joinLobbyButton.addEventListener("click", function () {
               )
                 .then((response) => response.json())
                 .then((array) => {
-                  console.log("XDKHJSDXHKCHSK", array);
                   fetch(
                     `https://deckofcardsapi.com/api/deck/${deckID}/pile/table/add/?cards=${array.cards[0].code}`,
                   );
@@ -199,20 +147,6 @@ joinLobbyButton.addEventListener("click", function () {
               playAreaDiv.hidden = false;
               setInterval(monitorNoJokersPile, 2000);
             });
-          // console.log(`Drawing ${startingHandSize} cards for both players`);
-          // fetch(
-          //   `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=${startingHandSize}`,
-          // )
-          //   .then((response) => response.json())
-          //   .then((array) => {
-          //     let drawnCards = [];
-          //     array.cards.forEach((e) => {
-          //       drawnCards.push(e.code);
-          //     });
-          //     fetch(
-          //       `https://deckofcardsapi.com/api/deck/${deckID}/pile/p1/add/?cards=${drawnCards.join(',')}`,
-          //     );
-          //   });
         });
     });
 });
@@ -222,37 +156,25 @@ createLobbyButton.addEventListener("click", function () {
   fetch(`https://deckofcardsapi.com/api/deck/new/?jokers_enabled=true`)
     .then((response) => response.json())
     .then((array) => {
-      console.log("Creating new deck");
       startingHandSize = handSizeInput.value;
-      console.log(array);
       deckID = array.deck_id;
       createdLobbyCode.innerHTML = `Lobby created with code: ${deckID}`;
       fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=54`)
         .then((response) => response.json())
         .then((array) => {
-          console.log("Drawing all cards from deck");
-          console.log(array);
           fetch(
             `https://deckofcardsapi.com/api/deck/${deckID}/pile/no_jokers/add/?cards=X1,X2`,
           )
             .then((response) => response.json())
             .then((array) => {
-              console.log("Adding jokers to no_jokers pile");
-              console.log(array);
               fetch(
                 `https://deckofcardsapi.com/api/deck/${deckID}/pile/jokers/add/?cards=X1,X2`,
               )
                 .then((response) => response.json())
                 .then((array) => {
-                  console.log("Moving jokers to jokers pile");
-                  console.log(array);
                   fetch(`https://deckofcardsapi.com/api/deck/${deckID}/return`)
                     .then((response) => response.json())
                     .then((array) => {
-                      console.log(
-                        `Returning other cards to deck, drawing ${startingHandSize} cards and waiting for other player to join`,
-                      );
-                      console.log(array);
                       fetch(
                         `https://deckofcardsapi.com/api/deck/${deckID}/shuffle/?remaining=true`,
                       ).then((response) => {
@@ -290,23 +212,17 @@ function monitorNoJokersPile() {
   fetch(`https://deckofcardsapi.com/api/deck/${deckID}/pile/no_jokers/list`)
     .then((response) => response.json())
     .then((array) => {
-      // console.log("Checking no_jokers pile");
-      // console.log(array);
       if (array.piles.jokers.remaining == 0) {
         turnText.innerHTML = "Opponent wins";
         myTurn = 1;
         return;
       }
       let nextTurn = array.piles.no_jokers.remaining;
-      // console.log(nextTurn);
       if (nextTurn) {
-        console.log("My turn now!");
         turnText.innerHTML = "Your turn";
         fetch(`https://deckofcardsapi.com/api/deck/${deckID}/pile/table/list`)
           .then((response) => response.json())
           .then((array) => {
-            console.log("Updating card");
-            console.log(array);
             if (!array.piles.table) return;
             pileCard.src = `https://deckofcardsapi.com/static/img/${array.piles.table.cards[array.piles.table.cards.length - 1].code}.png`;
           });
@@ -315,12 +231,7 @@ function monitorNoJokersPile() {
         myTurn = 1;
         fetch(
           `https://deckofcardsapi.com/api/deck/${deckID}/pile/jokers/add/?cards=X1`,
-        )
-          .then((response) => response.json())
-          .then((array) => {
-            console.log("Removed joker");
-            console.log(array);
-          });
+        );
       }
     });
 }
